04c07df9c68523bbf8c33a18c063efd4fdc3081f
rodolfoocampo <j.rodolfo.ocampo@gmail.com>
2017-05-08 18:58:56 -0500

    Added Information Gain function, not sure if working 100%

--- ID3.R
+++ ID3.R
@@ -1,7 +1,7 @@
 library(readr)
 library(dplyr)
 
-vector <- c("a","c","c","c","c")
+mtcars
 length(unique(vector))
 
 entropy <- function(vector){
@@ -16,9 +16,38 @@ entropy <- function(vector){
    
     # h accumulates the variable's entropy
     h <- h + (log2(1/pi))*pi
-    print(h)
   }
+  h
 }
 
+mtcars
 
+whole_data <- mtcars
+# dummy variable for testing -> will remove
+target_colname <- 'mpg'
+attribute_colname <- 'cyl'
+
+
+information_gain <- function(attribute_colname, target_colname, whole_data){
+  # calculate entropy for target vector
+  target_entropy <- entropy(whole_data[,target_colname])
+  target_entropy_i <- 0
+  attribute_factors <- as.factor(whole_data[,attribute_colname])
+  total <- length(attribute_factors)
+  entropy_i_sum <- 0
+  for(i in levels(attribute_factors)){
+    print(i)
+    subset_i <- filter(whole_data, whole_data[,attribute_colname] == i)
+    target_entropy_i <- entropy(subset_i[,target_colname])
+    print(target_entropy_i)
+    class_sum <- sum(attribute_factors == i)
+    pi <- class_sum/total
+    # Calculamos la probabilidad y la multiplicamos por la entropia del target en el subset
+    entropy_i_sum <- entropy_i_sum + target_entropy_i*pi 
+  }
+  IG <- target_entropy - entropy_i_sum
+  IG
+}
+
+ig <- information_gain('gear', 'mpg', mtcars)
 
